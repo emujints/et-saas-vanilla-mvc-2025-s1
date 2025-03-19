fae1031185f982b0f9fcdf42051f75b0f9ae3a81
Emujin <20126220@tafe@wa.edu.au>
2025-03-19 09:45:03 +0800

    wip: Commence Framework - Create the db.php cpnfig file  - Create helper.php file with functions

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Database Configuration File
+ *
+ * Filename:        db.php
+ * Location:        config/
+ * Project:         ET-SaaS-Vanilla-MVC-SN
+ * Date Created:    12-3-2025
+ *
+ * Author:          Emujin Tsengelbayar
+ *
+ */
+return [
+    'host' => '127.0.0.1',
+    'port' => 3306,
+    'dbname' => 'et_saas_vanilla_mvc',
+    'username' => 'et_saas_vanilla_mvc',
+    'password' => 'Password1234',
+];
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'emujints/et-saas-vanilla-mvc-2025-s1',
+        'pretty_version' => '0.1',
+        'version' => '0.1.0.0',
+        'reference' => NULL,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'emujints/et-saas-vanilla-mvc-2025-s1' => array(
+            'pretty_version' => '0.1',
+            'version' => '0.1.0.0',
+            'reference' => NULL,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
